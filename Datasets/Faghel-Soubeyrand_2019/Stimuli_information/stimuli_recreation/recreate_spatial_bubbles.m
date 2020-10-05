31980b93946eb0b224fe84eab628e111abe212aa
simonsoubeyrand <simonsoubeyrand@gmail.com>
2020-10-05 23:12:22 +0100

    cleared the code a bit

--- Datasets/Faghel-Soubeyrand_2019/Stimuli_information/stimuli_recreation/recreate_spatial_bubbles.m
+++ Datasets/Faghel-Soubeyrand_2019/Stimuli_information/stimuli_recreation/recreate_spatial_bubbles.m
@@ -7,52 +7,59 @@ stim_creat_dir    = fullfile(dataset_dir,'/Stimuli_information/stimuli_recreatio
 cd(stim_creat_dir); % cd to appropriate path
 addpath(utils_dir)  % add useful functions
 
+
+% this contains all the datasets` json files name, and folder, etc.
 dataset   = dir(fullfile(dataset_dir,'*/*.json')); % list all .json files exactly one directory under "dataset_dir" folder
 
-sub_dir   = fullfile(dataset(sub).folder,dataset(sub).name);
-
-% openjson open the subjects' files is really not optimal. I've done the
-% same work in about more than 10 times less code,using only matlab
-% built-in functions.
-% openjson(sub_dir) is now depricated.
-subj_data=jsondecode(fileread(sub_dir));
-
+% some constants
+nsubjects=length(dataset);
 img_size=[128,128];
 sigma=3;
 bubble = get_bubble(sigma);
 
-tmp_mask=zeros(img_size);
+for sub=1%:nsubjects
+    sub_dir   = fullfile(dataset(sub).folder,dataset(sub).name);
+    % openjson open the subjects' files is really not optimal. I've done the
+    % same work in about more than 10 times less code,using only matlab
+    % built-in functions.
+    % openjson(sub_dir) is now depricated.
+    subj_data=jsondecode(fileread(sub_dir));
+    
+    ntrials=length(subj_data.PreInduction.Bubbles_i);
 
-i_ind=subj_data.PreInduction.Bubbles_i{trial};
-j_ind=subj_data.PreInduction.Bubbles_j{trial};
-
-tmp_mask(j_ind,i_ind)=1;
-
-
-IND = sub2ind([128*128,1],i_ind,j_ind)
-
-masque2D=filter2(bubble,tmp_mask);
-figure, subplot(1,2,1),imshow(tmp_mask),title('bubbles center, supposedly')
-subplot(1,2,2), imshow(masque2D),title('bubbles mask, supposedly')
+    % test with PreInduction trials
+    for trial=1%:ntrials
         
+        tmp_mask=zeros(img_size);
+        
+        i_ind=subj_data.PreInduction.Bubbles_i{trial}; % problem with the indices here!
+        j_ind=subj_data.PreInduction.Bubbles_j{trial};
+        
+        tmp_mask(j_ind,i_ind)=1;
+        
+        % IND = sub2ind([128*128,1],i_ind,j_ind)
+  
+        masque2D=filter2(bubble,tmp_mask);
+    end
+    figure, subplot(1,2,1),imshow(tmp_mask),title('bubbles center, supposedly')
+    subplot(1,2,2), imshow(masque2D),title('bubbles mask, supposedly')
+end
 %% Get bubbles apertures function
 
-sizeX    = 128;
-masque2D = double(imread('masque.tif'));
-masque   = masque2D(:);
+% sizeX    = 128;
+% masque2D = double(imread('masque.tif'));
+% masque   = masque2D(:);
 
-
-
-function [bubbles_center,masque2D] = get_mask(nb_bubbles,masque)
-
-% Bulles
-sigma  = 3;
-bulle  = get_bubble(sigma);
-
-prob_tmp = nb_bubbles/sum(masque(:)); % the probability of having a bubble at a specific position the mask for this given trial
-tmp=rand(128^2,1) .* masque(:);
-bubbles_center=reshape(tmp>=(1-prob_tmp),128,128); % makes the criteria probabilistic :every position with  >=prob_temp gets a bubble.
-masque2D=filter2(bulle,tmp); % convolve these position centers with a gaussian apertures, or bubbles.
-masque2D = (masque2D - min(masque2D(:)))/(max(masque2D(:)) - min(masque2D(:))); % make it range from 0 to 1.
-
-end
+% function [bubbles_center,masque2D] = get_mask(nb_bubbles,masque)
+% 
+% % Bulles
+% sigma  = 3;
+% bulle  = get_bubble(sigma);
+% 
+% prob_tmp = nb_bubbles/sum(masque(:)); % the probability of having a bubble at a specific position the mask for this given trial
+% tmp=rand(128^2,1) .* masque(:);
+% bubbles_center=reshape(tmp>=(1-prob_tmp),128,128); % makes the criteria probabilistic :every position with  >=prob_temp gets a bubble.
+% masque2D=filter2(bulle,tmp); % convolve these position centers with a gaussian apertures, or bubbles.
+% masque2D = (masque2D - min(masque2D(:)))/(max(masque2D(:)) - min(masque2D(:))); % make it range from 0 to 1.
+% 
+% end
