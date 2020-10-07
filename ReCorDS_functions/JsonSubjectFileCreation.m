c3976dc9aa89ab6b1b5d7b1dcda8d9d8cae917c6
vdaigneault <valerie.daigneault.2@umontreal.ca>
2020-10-07 14:40:00 -0400

    Create JsonSubjectFileCreation.m

---
+++ ReCorDS_functions/JsonSubjectFileCreation.m
@@ -0,0 +1,22 @@
+% How to create subject JSON files
+% 1) Create the final structure wanted with all subject information for all
+% subjects (subject(ii).blocname.variablename). All variables in the same bloc should have the same length (number of trials in bloc)
+% (i.e. variable1 : 300 x 1, variable2 300 x1,...)
+% 2) Run this code
+%
+% Valerie Daigneault(valerie.daigneault.2@umontreal.ca),07/10/2020
+
+nbSubjects = 1;
+mainDir = "C:\Users\valer\Documents\GitHub\ReCorDS\Datasets\Faghel-Soubeyrand_2019";
+filename = "DataBehavior_";
+structure = nameOfStructureVariable;
+
+for ii = 1: nbSubjects
+     % Uncomment if new folders for subjects have not been made
+%     mkdir("C:\Users\valer\Documents\GitHub\ReCorDS\Datasets\Faghel-Soubeyrand_2019", "Subject_" +ii);
+    
+    cd(mainDir + "Subject_" + ii);
+    id = fopen(filename+ ii + ".json", 'w');
+    fprintf(id, '%s', jsonencode(structure(ii)));
+    fclose(id); 
+end
