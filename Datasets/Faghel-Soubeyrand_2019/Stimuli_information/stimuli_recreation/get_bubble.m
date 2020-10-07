1d7ee932f685b9bb37e1716f72d61435c8fe688f
simonsoubeyrand <simonsoubeyrand@gmail.com>
2020-10-07 18:20:00 +0100

    cleared up a lot of things for the creation of the tsv files (mostly in BubbleCentersVD); also chagned the recreation part

--- Datasets/Faghel-Soubeyrand_2019/Stimuli_information/stimuli_recreation/get_bubble.m
+++ Datasets/Faghel-Soubeyrand_2019/Stimuli_information/stimuli_recreation/get_bubble.m
@@ -1,7 +1,11 @@
-function bubble = get_bubble(sigma)
+function bulle = get_bubble(sigma)
 % Bulles
-TNoyau      = 6*sigma;
-bubble      = fspecial('gaussian',ceil(TNoyau),sigma);
-bubble      = bubble  - min(bubble(:));
-bubble      = bubble /sum(bubble(:));
+% TNoyau      = 6*sigma;
+% bubble      = fspecial('gaussian',ceil(TNoyau),sigma);
+% bubble      = bubble  - min(bubble(:));
+% bubble      = bubble /sum(bubble(:));
+
+maxHalfSize = 6*sigma;
+[y,x] = meshgrid(-maxHalfSize:maxHalfSize,-maxHalfSize:maxHalfSize);
+bulle = exp(-(x.^2/stdev^2)-(y.^2/stdev^2));
 end
